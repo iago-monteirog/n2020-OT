6dc9266c7d31c06137e51922bae0ed3718abd2d1
Iago Garcia <iagomgarcia9000@gmail.com>
2020-06-12 20:43:15 -0300

    arruma as rotas do Login

--- src/routes.js
+++ src/routes.js
@@ -5,9 +5,9 @@ const UsuarioController = require("./controller/UsuarioController");
 const CursoController = require("./controller/CursoController");
 const LoginController = require("./controller/LoginController");
 
-routes.post('/usuario', UsuarioController.create)
-routes.post('/curso/criar', CursoController.create)
-routes.get('/cursos', CursoController.index)
-routes.post("/login", LoginControlller.login)
+routes.post('/usuario', UsuarioController.create);
+routes.post('/curso/criar', CursoController.create);
+routes.get('/cursos', CursoController.index);
+routes.post("/login", LoginController.login);
 
 module.exports = routes;
