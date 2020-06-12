bc45dcfea4b2e9cb0b190a4e9a0cac51d736dfc9
Iago Garcia <iagomgarcia9000@gmail.com>
2020-06-12 20:42:24 -0300

    faz as rotas do Login

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,11 @@ const routes = express.Router();
 
 const UsuarioController = require("./controller/UsuarioController");
 const CursoController = require("./controller/CursoController");
+const LoginController = require("./controller/LoginController");
 
 routes.post('/usuario', UsuarioController.create)
-
-
 routes.post('/curso/criar', CursoController.create)
 routes.get('/cursos', CursoController.index)
+routes.post("/login", LoginControlller.login)
 
 module.exports = routes;
