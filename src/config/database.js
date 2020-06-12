3e4767a63d99bbc95032caf835bd3329660e42d0
Iago Garcia <iagomgarcia9000@gmail.com>
2020-06-12 20:39:29 -0300

    faz o Login

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
     dialect: "mysql",
     host: "localhost",
     username: "root",
-    password: "123456",
+    password: "",
     database: "n2020-ot",
     define: {
         timestamps: false,
---
+++ src/controller/LoginController.js
@@ -0,0 +1,27 @@
+const Usuario = require('../models/usuario');
+const jwt = require('jsonwebtoken');
+
+const authConfig = require('../config/auth.json');
+
+module.exports = {
+    async login(request, response) {
+        const {username, password} = request.body;
+
+        const user = await Usuario.findOne({
+            where: {
+                username: username,
+                password: password
+            }  
+        });
+
+        if(!user) return response.send({erro: {message: "Usuário não encontrado"}});
+
+        const token = jwt.sign({id: user.id}, authConfig.secret, {
+            expiresIn: 86400,
+        });
+
+        response.json({user, token});
+            
+    }
+}
+
