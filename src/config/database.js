7b3b4a459f7597e47214036c98520a4748856b82
Iago Garcia <iagomgarcia9000@gmail.com>
2020-06-16 20:50:27 -0300

    retira dialect

--- src/config/database.js
+++ src/config/database.js
@@ -12,7 +12,6 @@ module.exports = {
     },
 
     "production": {
-        dialect: "mysql",
         host: "us-cdbr-east-05.cleardb.net",
         username: "b708c3ad6388b7",
         password: "9a33a6ae",
