675e7033c31436f02eade9e2c57d57735bf1ef4e
Rodrigo Tanaka <rstanaka@aluscseduca.com.br>
2020-06-17 00:38:37 -0300

    Atualiza db config

--- src/config/database.js
+++ src/config/database.js
@@ -1,25 +1,32 @@
 module.exports = {
-    "development": {
-        dialect: "mysql",
-        host: "localhost",
-        username: "root",
-        password: "123456",
-        database: "n2020-ot",
-        define: {
-            timestamps: false,
-            underscored: false,
-        }
-    },
-
-    "production": {
-        dialect: "mysql",
-        host: "us-cdbr-east-05.cleardb.net",
-        username: "b70bb72ec449ee",
-        password: "2b327bf6",
-        database: "heroku_b73f9900251b5df",
-        define: {
-            timestamps: false,
-            underscored: false,
-        }
-    }
+  "development": {
+    "username": "root",
+    "password": "123456",
+    "database": "n2020-ot",
+    "host": "localhost",
+    "dialect": "mysql",
+    "define": {
+      "timestamps": false,
+      "underscored": false,
+  }
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "b70bb72ec449ee",
+    "password": "2b327bf6",
+    "database": "heroku_b73f9900251b5df",
+    "host": "us-cdbr-east-05.cleardb.net",
+    "dialect": "mysql",
+    "define": {
+      "timestamps": false,
+      "underscored": false,
+  }
+  }
 }
