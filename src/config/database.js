d9cbefe6c68dceaebf021b96595d23beaa0da89d
Rodrigo Tanaka <rstanaka@aluscseduca.com.br>
2020-06-16 22:21:47 -0300

    Atualiza senha

--- src/config/database.js
+++ src/config/database.js
@@ -15,7 +15,7 @@ module.exports = {
         dialect: "mysql",
         host: "us-cdbr-east-05.cleardb.net",
         username: "b708c3ad6388b7",
-        password: "9a33a6ae",
+        password: "c133bd66d03cebf",
         database: "heroku_2458ec383120ab7",
         define: {
             timestamps: false,
